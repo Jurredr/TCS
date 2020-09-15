36d20fe89198d65ab38af32846c31a3b69682d38
BuildTools <unconfigured@null.spigotmc.org>
2020-09-15 11:58:00 +0200

    nickname on join / quit

--- src/main/java/me/jurre/tcs/listener/JoinQuitListener.java
+++ src/main/java/me/jurre/tcs/listener/JoinQuitListener.java
@@ -23,30 +23,43 @@ public class JoinQuitListener implements Listener {
         Player player = e.getPlayer();
         UUID uuid = player.getUniqueId();
 
-        if (player.hasPlayedBefore()) {
-            e.setJoinMessage("§8[§a+§8] §7" + player.getDisplayName());
+        TcsPlayer tcsPlayer;
+        if (plugin.getPlayerDataFile().containsData(uuid)) {
+            tcsPlayer = plugin.getPlayerDataFile().getTcsPlayerData(player);
+            plugin.getPlayerDataManager().addOnlineTcsPlayer(uuid, tcsPlayer);
         } else {
-            e.setJoinMessage("§8[§a+§8] §7Welcome to the TCS server for the first time §a" + player.getDisplayName() + "§7!");
+            tcsPlayer = plugin.getPlayerDataFile().createPlayerData(player);
+            plugin.getPlayerDataManager().addOnlineTcsPlayer(uuid, tcsPlayer);
         }
 
-        if (plugin.getPlayerDataFile().containsData(uuid)) {
-            TcsPlayer tcsPlayer = plugin.getPlayerDataFile().getTcsPlayerData(player);
-            plugin.getPlayerDataManager().addOnlineTcsPlayer(uuid, tcsPlayer);
-        } else {
-            TcsPlayer tcsPlayer = plugin.getPlayerDataFile().createPlayerData(player);
-            plugin.getPlayerDataManager().addOnlineTcsPlayer(uuid, tcsPlayer);
+        String name = player.getName();
+        if (tcsPlayer.hasNickname()) {
+            name = player.getName() + " (" + tcsPlayer.getNickname() + ")";
         }
+
+        if (player.hasPlayedBefore()) {
+            e.setJoinMessage("§8[§a+§8] §7" + name);
+        } else {
+            e.setJoinMessage("§8[§a+§8] §7Welcome to the TCS server for the first time §a" + name + "§7!");
+        }
+
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
         Player player = e.getPlayer();
-        e.setQuitMessage("§8[§c-§8] §7" + player.getDisplayName());
+        String name = player.getName();
 
         if (plugin.getPlayerDataManager().isOnlineTcsPlayer(player.getUniqueId())) {
+            if (plugin.getPlayerDataManager().getOnlineTcsPlayer(player.getUniqueId()).hasNickname()) {
+                name = player.getName() + " (" + plugin.getPlayerDataManager().getOnlineTcsPlayer(player.getUniqueId()).getNickname() + ")";
+            }
+
             plugin.getPlayerDataManager().onPlayerQuit(player);
         } else {
             plugin.getServer().getConsoleSender().sendMessage("§4ERROR: Couldn't save player-data for player " + player.getName() + "!");
         }
+
+        e.setQuitMessage("§8[§c-§8] §7" + name);
     }
 }
